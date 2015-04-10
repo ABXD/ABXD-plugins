39924a14a813fac7d0480a9c4cb52fea0ef3f2e3
AlphaT3ch <AlphaT3ch@icloud.com>
2015-04-09 20:09:40 -0400

    Added Dirbaio's code

--- plugins/usernuke/page_nuke.php
+++ plugins/usernuke/page_nuke.php
@@ -45,12 +45,7 @@ if(isset($_POST["currpassword"]))
 		query("delete from {usercomments}
 				where uid={0} or cid={0}", $uid);
 		//Delete all of their private messages
-		$delID = query("select id from {pmsgs} where userfrom={0}", $uid);
-		if ($result->num_rows > 0) {
-			while($delID = $toDelID->fetch_assoc()) {
-			query("delete from {pmsgs_text} where pid={0}", $todelID);
-			}
-		} else {}
+		$delID = query("DELETE pt FROM pmsgs_text LEFT JOIN pmsgs p ON pt.pid = p.id WHERE p.userfrom = {0}", $uid);
 		query("delete from {pmsgs} where userfrom={0}", $uid);
 
 		//Delete THE USER ITSELF
